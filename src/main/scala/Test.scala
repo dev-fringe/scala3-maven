78dd111a88650bbf41d1528a3137cb4f8ff8c622
hdlee <dev.fringe@gmail.com>
2022-05-17 23:33:17 +0900

    is it simple?

--- src/main/scala/Test.scala
+++ src/main/scala/Test.scala
@@ -1,12 +1,7 @@
 import org.springframework.context.annotation.{AnnotationConfigApplicationContext, Configuration}
-
 import javax.annotation.PostConstruct
 
 @main def i = new AnnotationConfigApplicationContext(classOf[Test])
-
-@Configuration
-class Test {
-
-  @PostConstruct
-  def init() = print("test")
+@Configuration class Test {
+  @PostConstruct def init() = print("test")
 }
