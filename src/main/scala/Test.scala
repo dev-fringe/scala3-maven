9aafd7eea838650ab14a8d1974a70c77cc4e83e1
hdlee <dev.fringe@gmail.com>
2022-05-17 23:34:37 +0900

    is it simple?

--- src/main/scala/Test.scala
+++ src/main/scala/Test.scala
@@ -3,5 +3,5 @@ import javax.annotation.PostConstruct
 
 @main def i = new AnnotationConfigApplicationContext(classOf[Test])
 @Configuration class Test {
-  @PostConstruct def init() = print("test")
+  @PostConstruct def i = print("test")
 }
