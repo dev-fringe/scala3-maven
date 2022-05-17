d819e5b4324ef4a222172a1f2c73c099a72faca5
hdlee <dev.fringe@gmail.com>
2022-05-17 23:15:12 +0900

    initial commit

--- src/main/scala/Test.scala
+++ src/main/scala/Test.scala
@@ -1,4 +1,12 @@
-@main def i = println("sss")
+import org.springframework.context.annotation.{AnnotationConfigApplicationContext, Configuration}
+
+import javax.annotation.PostConstruct
+
+@main def i = new AnnotationConfigApplicationContext(classOf[Test])
+
+@Configuration
 class Test {
 
+  @PostConstruct
+  def init() = print("test")
 }
