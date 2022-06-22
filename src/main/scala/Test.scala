2d520f72796b560e920167ad63c4cbc8750f2ee3
hdlee <dev.fringe@gmail.com>
2022-06-22 18:52:09 +0900

    init

--- src/main/scala/Test.scala
+++ src/main/scala/Test.scala
@@ -1,4 +1,5 @@
 import org.springframework.boot.WebApplicationType
 import org.springframework.boot.builder.SpringApplicationBuilder
 //using boot for webpack build-image heroku and native maybe
+//TODO add need ant run scala3's @main
 @main def i = new SpringApplicationBuilder(classOf[Test2]).web(WebApplicationType.NONE).run()
