036be2d355678dacd59fc421d5f66802b15ec6d8
hdlee <dev.fringe@gmail.com>
2022-06-22 18:33:29 +0900

    init

--- src/main/java/Test2.java
+++ src/main/java/Test2.java
@@ -1,11 +1,28 @@
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.server.reactive.HttpHandler;
+import org.springframework.http.server.reactive.ReactorHttpHandlerAdapter;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.reactive.config.EnableWebFlux;
+import org.springframework.web.reactive.config.WebFluxConfigurer;
+import org.springframework.web.server.adapter.WebHttpHandlerBuilder;
+import reactor.netty.http.server.HttpServer;
 
 import javax.annotation.PostConstruct;
+import java.util.concurrent.TimeUnit;
 
-public class Test2 {
+@EnableWebFlux// not working .web(WebApplicationType.NONE)
+@Configuration
+@RestController
+public class Test2 implements WebFluxConfigurer{
 
-    @Autowire ApplicationContext context;
+    @Value("${server.port:8082}")
+    int port = 8082;
+    @Autowired ApplicationContext context;
 
     @PostConstruct
     public void init(){
@@ -13,5 +30,19 @@ public class Test2 {
         for (String bean : beans) {
             System.out.println(bean);
         }
+        context.getBean(HttpServer.class).bindNow().onDispose().block();
+    }
+
+    @Bean
+    public HttpServer nettyHttpServer(ApplicationContext context) {
+        HttpHandler handler = WebHttpHandlerBuilder.applicationContext(context).build();
+        ReactorHttpHandlerAdapter adapter = new ReactorHttpHandlerAdapter(handler);
+        HttpServer httpServer = HttpServer.create().host("localhost").port(this.port);
+        return httpServer.handle(adapter);
+    }
+
+    @RequestMapping("/")
+    public String hello() {
+        return "hello world";
     }
 }
--- src/main/scala/Test.scala
+++ src/main/scala/Test.scala
@@ -1,4 +1,6 @@
+import org.springframework.boot.WebApplicationType
 import org.springframework.boot.builder.SpringApplicationBuilder
-import javax.annotation.PostConstruct
 
-@main def i = new SpringApplicationBuilder(classOf[Test2]).run()
+import javax.annotation.PostConstruct
+//using boot for webpack build-image
+@main def i = new SpringApplicationBuilder(classOf[Test2]).web(WebApplicationType.NONE).run()
