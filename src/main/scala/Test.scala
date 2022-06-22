4b1d55790f59660c4b6d4b73d734ab67f22efd93
hdlee <dev.fringe@gmail.com>
2022-06-22 18:50:23 +0900

    init

--- src/main/scala/Test.scala
+++ src/main/scala/Test.scala
@@ -1,6 +1,4 @@
 import org.springframework.boot.WebApplicationType
 import org.springframework.boot.builder.SpringApplicationBuilder
-
-import javax.annotation.PostConstruct
 //using boot for webpack build-image heroku and native maybe
 @main def i = new SpringApplicationBuilder(classOf[Test2]).web(WebApplicationType.NONE).run()
